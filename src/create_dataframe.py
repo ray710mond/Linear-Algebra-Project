8872a3d2fac256e70db1877d8d6f7abbfbfb5101
Raymond Provost <ray710mond@gmail.com>
2023-02-13 09:47:52 -0500

    changed create_database to create_dataframe

--- src/create_dataframe.py
+++ src/create_dataframe.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 class Fangraphs:
-    def create_database(file_name):
+    def create_dataframe(file_name):
         directory = r"C:\Users\ray71\OneDrive\Documents\Regis Documents\Senior Year\Linear Algebra\Final Project\Final CSV Files"
         abs_path = os.path.join(directory, file_name)
         print(abs_path)
@@ -10,12 +10,12 @@ class Fangraphs:
             database = pd.read_csv(abs_path)
         return database
 
-    starters = create_database('Starting Pitchers - Fangraphs Leaderboards.csv')
-    relievers = create_database('Relief Pitchers - Fangraphs Leaderboards.csv')
-    catchers = create_database('Catchers - Fangraphs Leaderboards.csv')
-    first_basemen = create_database('First Basemen - Fangraphs Leaderboards.csv')
-    second_basemen = create_database('Second Basemen - Fangraphs Leaderboards.csv')
-    third_basemen = create_database('Third Basemen - Fangraphs Leaderboards.csv')
-    shortstops = create_database('Shortstops - Fangraphs Leaderboards.csv')
-    outfielders = create_database('Outfielders - Fangraphs Leaderboards.csv')
-    designated_hitters = create_database('Designated Hitters - Fangraphs Leaderboards.csv')
+    starters = create_dataframe('Starting Pitchers - Fangraphs Leaderboards.csv')
+    relievers = create_dataframe('Relief Pitchers - Fangraphs Leaderboards.csv')
+    catchers = create_dataframe('Catchers - Fangraphs Leaderboards.csv')
+    first_basemen = create_dataframe('First Basemen - Fangraphs Leaderboards.csv')
+    second_basemen = create_dataframe('Second Basemen - Fangraphs Leaderboards.csv')
+    third_basemen = create_dataframe('Third Basemen - Fangraphs Leaderboards.csv')
+    shortstops = create_dataframe('Shortstops - Fangraphs Leaderboards.csv')
+    outfielders = create_dataframe('Outfielders - Fangraphs Leaderboards.csv')
+    designated_hitters = create_dataframe('Designated Hitters - Fangraphs Leaderboards.csv')
